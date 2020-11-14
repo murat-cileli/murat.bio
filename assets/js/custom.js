4e2daeeb7bb5ce3b6a1c76ae87f1280fc5e2694a
Murat Çileli <murat.cileli@gmail.com>
2020-11-14 18:49:33 +0300

    Updates.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,15 +1,15 @@
 function cbReady() { 
     let pathName = window.location.pathname;      
 
-    if (pathName == '/' || pathName == '/index.html' || pathName == '/en/') {
+    if (pathName === '/' || pathName === '/index.html' || pathName === '/en/') {
         document.querySelector('.item-about').style.display = 'none';
     }
 
-    if (pathName == '/neler-yaptim.html' || pathName == '/en/portfolio.html') {
+    if (pathName === '/neler-yaptim.html' || pathName === '/en/portfolio.html') {
         document.querySelector('.item-portfolio').style.display = 'none';    
     }
 
-    if (pathName == '/basinda-projelerim.html' || pathName == '/en/in-the-press.html') {
+    if (pathName === '/basinda-projelerim.html' || pathName === '/en/in-the-press.html') {
         document.querySelector('.item-in-the-press').style.display = 'none'; 
     }
 }
